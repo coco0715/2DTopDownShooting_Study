42924c7a94d11f5ff9d8a49c7c6969a412903d56
coco0715 <ment0715@naver.com>
2023-10-05 19:49:58 +0900

    Feat: PlayerInputController 추가
    - 캐릭터의 움직임, 마우스 커서 방향 바라보기, 마우스 왼쪽 클릭 시 공격 할 입력 값 가져오는 기능 추가

---
+++ Assets/Scripts/Controllers/PlayerInputController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownCharacterController
+{
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+        //Debug.Log("OnMove" + value.ToString());
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        CallLookEvent(newAim);
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position.normalized);
+
+        if (newAim.magnitude >= .9f)
+        {
+            CallLookEvent(newAim);
+        }
+        //Debug.Log("OnLook" + value.ToString());
+    }
+
+    public void OnFire(InputValue value)
+    {
+        Debug.Log("OnFire" + value.ToString());
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
