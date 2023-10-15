114fac4a224d397094288490c4fed20516fbead9
coco0715 <ment0715@naver.com>
2023-10-15 12:23:00 +0900

    Feat: GameManager 코루틴 기능 추가
    - GameManager 스크립트 게임 로직 추가중
    - 플레이어 이동 오류 수정 후 이후 로직 진행
    - Fixes: #20

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -17,6 +16,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Slider hpGaugeSlider;
     [SerializeField] private GameObject gameOverUI;
 
+    [SerializeField] private int currentWaveIndex = 0;
+    private int currentSpawnCount = 0;
+    private int waveSpawnCount = 0;
+    private int waveSpawnPosCount = 0;
+
+    public float spawnInterval = .5f;
+    public List<GameObject> enemyPrefabs = new List<GameObject>();
+    [SerializeField] private Transform spawnPositionsRoot;
+    private List<Transform> spawnPositions = new List<Transform>();
+
     private void Awake()
     {
         instance = this;
@@ -28,20 +37,81 @@ public class GameManager : MonoBehaviour
         playerHealthSystem.OnDeath += GameOver;
 
         gameOverUI.SetActive(false);
+        for (int i = 0; i < spawnPositionsRoot.childCount; i++)
+        {
+            spawnPositions.Add(spawnPositionsRoot.GetChild(i));
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine("StartNextWave");
+    }
+
+    IEnumerator StartNextWave()
+    {
+        while (true)
+        {
+            if (currentSpawnCount == 0)
+            {
+                UpdateWaveUI();
+                yield return new WaitForSeconds(2f);
+                
+                if (currentWaveIndex % 10 == 0)
+                {
+                    waveSpawnPosCount = waveSpawnPosCount + 1 > spawnPositions.Count ? 1 : 0;
+                    waveSpawnCount = 0;
+                }
+
+                if (currentWaveIndex % 5 == 0)
+                {
+
+                }
+
+                if (currentWaveIndex % 3 == 0)
+                {
+                    waveSpawnCount += 1;
+                }
+
+                for (int i = 0; i < waveSpawnPosCount; i++)
+                {
+                    int posIdx = Random.Range(0, spawnPositions.Count);
+                    for (int j = 0; j < waveSpawnCount; j++)
+                    {
+                        int prefabIdx = Random.Range(0, enemyPrefabs.Count);
+                        GameObject enemy = Instantiate(enemyPrefabs[prefabIdx], spawnPositions[posIdx].position, Quaternion.identity);
+                        enemy.GetComponent<HealthSystem>().OnDeath += OnEnemyDeath;
+                        //enemy.GetComponent<CharacterStatsHendler>()
+                        currentSpawnCount++;
+                        yield return new WaitForSeconds(spawnInterval);
+                    }
+                }
+                currentWaveIndex++;
+            }
+
+            yield return null;
+        }
+    }
+
+    private void OnEnemyDeath()
+    {
+        currentSpawnCount--;
     }
 
     private void UpdateHealthUI()
     {
         hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
     }
+
     private void GameOver()
     {
         gameOverUI.SetActive(true);
+        StopAllCoroutines();
     }
 
     private void UpdateWaveUI()
     {
-
+        waveText.text = (currentWaveIndex + 1).ToString();
     }
 
     public void RestartGame()
